474fc9d54af27328739f766603823a9e3c366547
Patrick Kubiak <pk9948@rit.edu>
2021-12-02 11:33:16 -0500

    format with black

--- day01/day01.py
+++ day01/day01.py
@@ -2,27 +2,28 @@
 
 import sys
 
+
 def count_increased(f):
-  # how many measurements increased
-  increased = 0
-  # read first line
-  previous = int(f.readline().strip())
-  # read the rest of the file line-by-line
-  for line in f:
-    current = int(line.strip())
-    if current > previous:
-      increased += 1
-    previous = current
-  return increased
+    # how many measurements increased
+    increased = 0
+    # read first line
+    previous = int(f.readline().strip())
+    # read the rest of the file line-by-line
+    for line in f:
+        current = int(line.strip())
+        if current > previous:
+            increased += 1
+        previous = current
+    return increased
+
 
 if __name__ == "__main__":
-  # parse arguments
-  if len(sys.argv) < 1:
-    print("Usage: ./day01.py input")
-    exit(1)
+    # parse arguments
+    if len(sys.argv) < 1:
+        print("Usage: ./day01.py input")
+        exit(1)
 
-  input_file = sys.argv[1]
-  with open(input_file) as f:
-    increased = count_increased(f)
-    print(increased)
-
+    input_file = sys.argv[1]
+    with open(input_file) as f:
+        increased = count_increased(f)
+        print(increased)
