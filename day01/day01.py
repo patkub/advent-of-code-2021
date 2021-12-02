52b60f71f6702214ca830caba169abd6c6dd37e2
Patrick Kubiak <pk9948@rit.edu>
2021-12-02 14:24:49 -0500

    use list of values instead of file input

--- day01/day01.py
+++ day01/day01.py
@@ -3,34 +3,31 @@
 import sys
 
 
-def count_increased(f):
+def count_increased(values):
     # how many measurements increased
     increased = 0
-    # read first line
-    previous = int(f.readline().strip())
-    # read the rest of the file line-by-line
-    for line in f:
-        current = int(line.strip())
+    # first value
+    previous = values[0]
+    # rest of the values
+    for current in values[1:]:
         if current > previous:
             increased += 1
         previous = current
     return increased
 
 
-def count_increased_part2(f):
+def count_increased_part2(values):
     # how many measurements increased in three-measurement sliding window
     increased = 0
-    values = [int(line) for line in f.readlines()]
-
     for i in range(2, len(values) - 1, 1):
-      #print(str(values[i - 2]) + ", " + str(values[i - 1]) + ", " + str(values[i]))
-      #print(str(values[i - 1]) + ", " + str(values[i]) + ", " + str(values[i + 1]) + "\n")
-      window1 = values[i - 2] + values[i - 1] + values[i]
-      window2 = values[i - 1] + values[i] + values[i + 1]
-      if window2 > window1:
-        increased += 1
-
+        # print(str(values[i - 2]) + ", " + str(values[i - 1]) + ", " + str(values[i]))
+        # print(str(values[i - 1]) + ", " + str(values[i]) + ", " + str(values[i + 1]) + "\n")
+        window1 = values[i - 2] + values[i - 1] + values[i]
+        window2 = values[i - 1] + values[i] + values[i + 1]
+        if window2 > window1:
+            increased += 1
     return increased
+
 
 if __name__ == "__main__":
     # parse arguments
@@ -40,8 +37,9 @@ if __name__ == "__main__":
 
     input_file = sys.argv[1]
     with open(input_file) as f:
-        increased = count_increased(f)
-        print(increased)
-        f.seek(0)
-        increased = count_increased_part2(f)
-        print(increased)
+        values = [int(line) for line in f.readlines()]
+
+    increased = count_increased(values)
+    print(increased)
+    increased = count_increased_part2(values)
+    print(increased)
--- day01/test_day01.py
+++ day01/test_day01.py
@@ -4,17 +4,20 @@ from day01 import count_increased, count_increased_part2
 
 def test_example_report():
     with open("./day01/example-report") as f:
-        increased = count_increased(f)
+        values = [int(line) for line in f.readlines()]
+        increased = count_increased(values)
         assert increased == 7
 
 
 def test_part1():
     with open("./day01/input") as f:
-        increased = count_increased(f)
+        values = [int(line) for line in f.readlines()]
+        increased = count_increased(values)
         assert increased == 1722
 
 
 def test_part2():
     with open("./day01/input") as f:
-        increased = count_increased_part2(f)
+        values = [int(line) for line in f.readlines()]
+        increased = count_increased_part2(values)
         assert increased == 1748
