eb407e76265361254066d8cf3c6aa67cc0b26f68
Patrick Kubiak <pk9948@rit.edu>
2021-12-02 11:55:27 -0500

    day2 part2

--- day02/day02.py
+++ day02/day02.py
@@ -23,6 +23,31 @@ def calc_horizontal_depth(f):
     return horizontal * depth
 
 
+def calc_horizontal_depth_part2(f):
+    horizontal, depth, aim = 0, 0, 0
+
+    # read the rest of the file line-by-line
+    for line in f:
+        command = line.strip().split(" ")
+        # command = ["forward", "5"]
+        if len(command) >= 2:
+            units = int(command[1])
+            if command[0] == "forward":
+                # increases horizontal position by X units
+                horizontal += units
+                # increases depth by "aim * X" units
+                depth += aim * units
+            elif command[0] == "down":
+                # increases aim by X units
+                aim += units
+            elif command[0] == "up":
+                # decreases aim by X units
+                aim -= units
+
+    # final horizontal position multiplied by final depth
+    return horizontal * depth
+
+
 if __name__ == "__main__":
     # parse arguments
     if len(sys.argv) < 1:
@@ -32,4 +57,7 @@ if __name__ == "__main__":
     input_file = sys.argv[1]
     with open(input_file) as f:
         res = calc_horizontal_depth(f)
+        print(res)
+        f.seek(0)
+        res = calc_horizontal_depth_part2(f)
         print(res)
--- day02/test_day02.py
+++ day02/test_day02.py
@@ -1,8 +1,14 @@
 import pytest
-from day02 import calc_horizontal_depth
+from day02 import calc_horizontal_depth, calc_horizontal_depth_part2
 
 
-def test_input():
+def test_horizontal_depth():
     with open("./day02/input") as f:
         res = calc_horizontal_depth(f)
         assert res == 1660158
+
+
+def test_horizontal_depth_part2():
+    with open("./day02/input") as f:
+        res = calc_horizontal_depth_part2(f)
+        assert res == 1604592846
