76422b17eea68bfb2901099ca291d804aa3a51a0
Patrick Kubiak <pk9948@rit.edu>
2021-12-03 03:33:17 -0500

    day3 part 2: life support rating

--- day03/day03.py
+++ day03/day03.py
@@ -42,6 +42,73 @@ def calc_power_consumption(lines):
     return power_consumption
 
 
+def calc_oxygen_generator_rating(lines):
+    """
+    Calculate oxygen generator rating
+
+    :param lines: List of lines from file
+    :returns: Oxygen generator rating
+    """
+    # oxygen generator rating
+    filtered_lines = lines.copy()
+    index = 0
+    while len(filtered_lines) > 1 and index < len(filtered_lines[0]):
+        bit_column = [int(l[index]) for l in filtered_lines]
+        num_zeros = bit_column.count(0)
+        num_ones = bit_column.count(1)
+        if num_zeros == num_ones:
+            # keep 1s
+            filtered_lines = [l for l in filtered_lines if int(l[index]) == 1]
+        elif num_zeros > num_ones:
+            # keep 0s
+            filtered_lines = [l for l in filtered_lines if int(l[index]) == 0]
+        else:
+            # keep 1s
+            filtered_lines = [l for l in filtered_lines if int(l[index]) == 1]
+        index += 1
+    oxygen_generator_rating = int(filtered_lines[0], 2)
+    return oxygen_generator_rating
+
+
+def calc_c02_scrubber_rating(lines):
+    """
+    Calculate CO2 scrubber rating
+
+    :param lines: List of lines from file
+    :returns: CO2 scrubber rating
+    """
+    # CO2 scrubber rating
+    filtered_lines = lines.copy()
+    index = 0
+    while len(filtered_lines) > 1 and index < len(filtered_lines[0]):
+        bit_column = [int(l[index]) for l in filtered_lines]
+        num_zeros = bit_column.count(0)
+        num_ones = bit_column.count(1)
+        if num_zeros == num_ones:
+            # keep 0s
+            filtered_lines = [l for l in filtered_lines if int(l[index]) == 0]
+        elif num_zeros < num_ones:
+            # keep 0s
+            filtered_lines = [l for l in filtered_lines if int(l[index]) == 0]
+        else:
+            # keep 1s
+            filtered_lines = [l for l in filtered_lines if int(l[index]) == 1]
+        index += 1
+    c02_scrubber_rating = int(filtered_lines[0], 2)
+    return c02_scrubber_rating
+
+
+def calc_life_support_rating(lines):
+    """
+    Calculate life support rating
+
+    :param lines: List of lines from file
+    :returns: Life support rating
+    """
+    # life support rating = oxygen generator rating * CO2 scrubber rating
+    return calc_oxygen_generator_rating(lines) * calc_c02_scrubber_rating(lines)
+
+
 if __name__ == "__main__":
     # parse arguments
     if len(sys.argv) < 1:
@@ -54,4 +121,6 @@ if __name__ == "__main__":
         lines = [line.strip() for line in f.readlines()]
 
     res = calc_power_consumption(lines)
+    print(res)
+    res = calc_life_support_rating(lines)
     print(res)
--- day03/test_day03.py
+++ day03/test_day03.py
@@ -1,5 +1,5 @@
 import pytest
-from day03 import most_common, calc_power_consumption
+from day03 import most_common, calc_power_consumption, calc_life_support_rating
 
 
 def test_example_power_consumption():
@@ -14,3 +14,17 @@ def test_power_consumption():
         lines = [line.strip() for line in f.readlines()]
         res = calc_power_consumption(lines)
         assert res == 775304
+
+
+def test_example_life_support_rating():
+    with open("./day03/example-input") as f:
+        lines = [line.strip() for line in f.readlines()]
+        res = calc_life_support_rating(lines)
+        assert res == 230
+
+
+def test_life_support_rating():
+    with open("./day03/input") as f:
+        lines = [line.strip() for line in f.readlines()]
+        res = calc_life_support_rating(lines)
+        assert res == 1370737
