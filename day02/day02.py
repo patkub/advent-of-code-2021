d1a5b34b2c61655dd52f8379dfd310f16353f981
Patrick Kubiak <pk9948@rit.edu>
2021-12-05 17:26:08 -0500

    day02 parse commands as tuples

--- day02/day02.py
+++ day02/day02.py
@@ -7,22 +7,20 @@ def calc_horizontal_depth(lines):
     """
     Calculate horizontal depth
 
-    :param lines: List of lines from file
+    :param lines: List of tuples of lines from file
     :returns: Horizontal position multiplied by final depth
     """
     horizontal, depth = 0, 0
 
-    for line in lines:
-        command = line.strip().split(" ")
-        # command = ["forward", "5"]
-        if len(command) >= 2:
-            units = int(command[1])
-            if command[0] == "forward":
-                horizontal += units
-            elif command[0] == "down":
-                depth += units
-            elif command[0] == "up":
-                depth -= units
+    for command in lines:
+        # command = ('forward', '5')
+        units = int(command[1])
+        if command[0] == "forward":
+            horizontal += units
+        elif command[0] == "down":
+            depth += units
+        elif command[0] == "up":
+            depth -= units
 
     # final horizontal position multiplied by final depth
     return horizontal * depth
@@ -32,27 +30,25 @@ def calc_horizontal_depth_part2(lines):
     """
     Calculate horizontal depth with aim
 
-    :param lines: List of lines from file
+    :param lines:List of tuples of lines from file
     :returns: Horizontal position multiplied by final depth
     """
     horizontal, depth, aim = 0, 0, 0
 
-    for line in lines:
-        command = line.strip().split(" ")
-        # command = ["forward", "5"]
-        if len(command) >= 2:
-            units = int(command[1])
-            if command[0] == "forward":
-                # increases horizontal position by X units
-                horizontal += units
-                # increases depth by "aim * X" units
-                depth += aim * units
-            elif command[0] == "down":
-                # increases aim by X units
-                aim += units
-            elif command[0] == "up":
-                # decreases aim by X units
-                aim -= units
+    for command in lines:
+        # command = ('forward', '5')
+        units = int(command[1])
+        if command[0] == "forward":
+            # increases horizontal position by X units
+            horizontal += units
+            # increases depth by "aim * X" units
+            depth += aim * units
+        elif command[0] == "down":
+            # increases aim by X units
+            aim += units
+        elif command[0] == "up":
+            # decreases aim by X units
+            aim -= units
 
     # final horizontal position multiplied by final depth
     return horizontal * depth
@@ -67,7 +63,7 @@ if __name__ == "__main__":
     input_file = sys.argv[1]
     # read file line-by-line
     with open(input_file) as f:
-        lines = [line.strip() for line in f.readlines()]
+        lines = [tuple(line.strip().split()) for line in f.readlines()]
 
     res = calc_horizontal_depth(lines)
     print(res)
--- day02/test_day02.py
+++ day02/test_day02.py
@@ -4,23 +4,27 @@ from day02 import calc_horizontal_depth, calc_horizontal_depth_part2
 
 def test_example_horizontal_depth():
     with open("./day02/example-input") as f:
-        res = calc_horizontal_depth(f)
+        lines = [tuple(line.strip().split()) for line in f.readlines()]
+        res = calc_horizontal_depth(lines)
         assert res == 150
 
 
 def test_example_horizontal_depth_part2():
     with open("./day02/example-input") as f:
-        res = calc_horizontal_depth_part2(f)
+        lines = [tuple(line.strip().split()) for line in f.readlines()]
+        res = calc_horizontal_depth_part2(lines)
         assert res == 900
 
 
 def test_horizontal_depth():
     with open("./day02/input") as f:
-        res = calc_horizontal_depth(f)
+        lines = [tuple(line.strip().split()) for line in f.readlines()]
+        res = calc_horizontal_depth(lines)
         assert res == 1660158
 
 
 def test_horizontal_depth_part2():
     with open("./day02/input") as f:
-        res = calc_horizontal_depth_part2(f)
+        lines = [tuple(line.strip().split()) for line in f.readlines()]
+        res = calc_horizontal_depth_part2(lines)
         assert res == 1604592846
